a4de4a5e218d98142d5151e6f464a966055df6ac
Kasumi102 <kasumi102@gmail.com>
2019-12-06 22:56:04 +0900

    first

---
+++ public/js/main.js
@@ -0,0 +1,50 @@
+"use strict";
+{
+  // 文字を押してURLに飛ばす
+  document.getElementById("arlink").addEventListener("click", function (evt) {
+    window.open("https://gsacademy.tokyo/", "_blank");
+  }, false);
+  // 1;
+  // 2;
+  // 3;
+  // 4;
+  // 5;
+  // 6;
+  // 7;
+  // 8;
+  // 9;
+  // 10;
+  // 11;
+  // 12;
+  // 13;
+  // 14;
+  // 15;
+  // 16;
+  // 17;
+  // 18;
+  // 19;
+  // 20;
+  // 21;
+  // $(".menu-trigger").on("click", function() {
+  //   if ($(this).hasClass("active")) {
+  //     $(this).removeClass("active");
+  //     $("main").removeClass("open");
+  //     $("nav").removeClass("open");
+  //     $(".overlay").removeClass("open");
+  //   } else {
+  //     $(this).addClass("active");
+  //     $("main").addClass("open");
+  //     $("nav").addClass("open");
+  //     $(".overlay").addClass("open");
+  //   }
+  // });
+
+  // $(".overlay").on("click", function() {
+  //   if ($(this).hasClass("open")) {
+  //     $(this).removeClass("open");
+  //     $(".menu-trigger").removeClass("active");
+  //     $("main").removeClass("open");
+  //     $("nav").removeClass("open");
+  //   }
+  // });
+}
